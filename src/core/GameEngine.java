154e650733fb3b3fa1f8586d14073736e8811a20
Nick Board <nboard94@gmail.com>
2018-09-27 22:40:15 -0400

    update to movement

--- src/core/GameEngine.java
+++ src/core/GameEngine.java
@@ -3,6 +3,7 @@ package core;
 import processing.core.PApplet;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class GameEngine extends PApplet {
 
@@ -10,6 +11,7 @@ public class GameEngine extends PApplet {
     private static java.util.UUID gameID;
 
     private Rectangle bounds;
+    private ArrayList<Rectangle> walls = new ArrayList<Rectangle>();
     private Rectangle r1;
 
     // up down left right
@@ -30,10 +32,20 @@ public class GameEngine extends PApplet {
     public void setup() {
         bounds = new Rectangle(0, 0, width, height);
         r1 = new Rectangle(0, 450, 50, 50);
+
+        walls.add(new Rectangle(250, 450, 100, 25));
+        walls.add(new Rectangle(150, 250, 100, 25));
     }
 
     public void draw() {
         background(151);
+
+        Rectangle cRec;
+        for(int i = 0; i < walls.size(); i++) {
+            cRec = walls.get(i);
+            rect((float) cRec.getX(), (float) cRec.getY(), (float) cRec.getWidth(), (float) cRec.getHeight());
+        }
+
         rect((float) r1.getX(), (float) r1.getY(), (float) r1.getWidth(), (float) r1.getHeight());
         moveRect();
     }
@@ -77,23 +89,65 @@ public class GameEngine extends PApplet {
 
     private void moveRect() {
 
-        Rectangle fRec = (Rectangle) r1.clone();
+        Rectangle fRec;
         int speed = 5;
 
         if(keys[0]) {
+            boolean move = true;
+            fRec = (Rectangle)r1.clone();
             fRec.translate(0, -speed);
+            if(!bounds.contains(fRec)) move = false;
+
+            Rectangle cRec;
+            for(int i = 0; i < walls.size(); i++) {
+                cRec = walls.get(i);
+                if(fRec.intersects(cRec)) move = false;
+            }
+
+            if(move) r1 = fRec;
         }
         if(keys[1]) {
+            boolean move = true;
+            fRec = (Rectangle)r1.clone();
             fRec.translate(0, +speed);
+            if(!bounds.contains(fRec)) move = false;
+
+            Rectangle cRec;
+            for(int i = 0; i < walls.size(); i++) {
+                cRec = walls.get(i);
+                if(fRec.intersects(cRec)) move = false;
+            }
+
+            if(move) r1 = fRec;
         }
         if(keys[2]) {
+            boolean move = true;
+            fRec = (Rectangle)r1.clone();
             fRec.translate(-speed, 0);
+            if(!bounds.contains(fRec)) move = false;
+
+            Rectangle cRec;
+            for(int i = 0; i < walls.size(); i++) {
+                cRec = walls.get(i);
+                if(fRec.intersects(cRec)) move = false;
+            }
+
+            if(move) r1 = fRec;
         }
         if(keys[3]) {
+            boolean move = true;
+            fRec = (Rectangle)r1.clone();
             fRec.translate(+speed, 0);
-        }
+            if(!bounds.contains(fRec)) move = false;
 
-        if(bounds.contains(fRec)) r1 = fRec;
+            Rectangle cRec;
+            for(int i = 0; i < walls.size(); i++) {
+                cRec = walls.get(i);
+                if(fRec.intersects(cRec)) move = false;
+            }
+
+            if(move) r1 = fRec;
+        }
     }
 
     private void gravity() {
